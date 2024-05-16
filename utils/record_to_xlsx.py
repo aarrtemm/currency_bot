0177b0f91d7d701d9fbec91eaaae6e8455d18539
aarrtemm <getapple@MacBook-Pro.local>
2024-05-16 14:25:10 +0300

    Implemented util to record an exchange rate in an Excel file

---
+++ utils/record_to_xlsx.py
@@ -0,0 +1,13 @@
+import openpyxl
+from openpyxl import Workbook
+
+
+def record_to_excel(exchange_rates):
+    wb = Workbook()
+    ws = wb.active
+    ws.append(["id", "datetime", "exchange_rate"])
+
+    for rate in exchange_rates:
+        ws.append([rate.id, rate.date, rate.rate])
+
+    wb.save(f"result.xlsx")
