74099dd4831b02dbb63ad7c8c426db373b27ca9e
aarrtemm <getapple@MacBook-Pro.local>
2024-05-17 13:18:04 +0300

    Added logging and the try/exept statements

--- parse.py
+++ parse.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import requests
 import lxml
@@ -11,32 +12,23 @@ TARGET_URL = f"{BASE_URL}/quote/{F_CURRENCY}-{S_CURRENCY}"
 
 
 def get_exchange_rate() -> float:
-    response = requests.get(TARGET_URL)
-
-    with open("page.html", "w") as file:
-        file.write(response.text)
-
-    with open("page.html") as file:
-        src = file.read()
-
-    soup = BeautifulSoup(src, "lxml")
-    price = soup.find("div", {"data-last-price": True})["data-last-price"]
-
-    delete_file()
-    return float(price)
+    try:
+        response = requests.get(TARGET_URL)
+        with open("page.html", "w") as file:
+            file.write(response.text)
+        with open("page.html") as file:
+            src = file.read()
+        soup = BeautifulSoup(src, "lxml")
+        price = soup.find("div", {"data-last-price": True})["data-last-price"]
+        delete_file()
+        return float(price)
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        return 0.0
 
 
 def delete_file():
     try:
         os.remove("page.html")
     except Exception as e:
-        print(e)
-
-
-def main():
-    get_exchange_rate()
-
-
-if __name__ == '__main__':
-    main()
-
+        logging.error(f"Failed to delete file: {e}")
